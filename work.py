36803733b857584ab301a942bbc7da7fc03e6b1a
ZionLieb <1838169875@qq.com>
2024-11-25 13:46:55 +0800

    修改了新版webdriver函数不适配的问题，在14题教材中统一选择没有教材，避免弹出新的选择题造成无法提交。修改一些其他bug

--- work.py
+++ work.py
@@ -10,61 +10,107 @@ print("请输入uis密码")
 password = input()
 '''
 
-usrname = ''
-password = ''
+usrname = '*****'
+password = '***'
 
 driver = webdriver.Chrome()
 url = "http://ce.fudan.edu.cn"
-
+from selenium.webdriver.common.by import By
 driver.get(url)
-usr = driver.find_element_by_id('username')
-pwd = driver.find_element_by_id('password')
-submit = driver.find_element_by_id('idcheckloginbtn')
+usr = driver.find_element(By.ID, 'username')
+pwd = driver.find_element(By.ID, 'password')
+submit = driver.find_element(By.ID, 'idcheckloginbtn')
+
 
 usr.send_keys(usrname)   #填入uis用户名
 pwd.send_keys(password)   #填入uis密码
 
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 submit.click()
-#driver.get("http://ce.fudan.edu.cn/q.aspx?id=9957450&beginTime=2020-04-24%2015:00&endTime=2020-06-20%2023:59&previewTime=2020-03-22%2008:00%20&sqid=b956f4a567ed4ab982a7aede07a4a6da&type=5&stepseq=45ff583eeca240959051a0bf2f11efee&targettype=2&targetcode=869a78ece4374719a3ee006f8661746b&tag=b956f4a567ed4ab982a7aede07a4a6da_5_869a78ece4374719a3ee006f8661746b_2_2019-2020-2-COMP130154_0_9957450&name=2020%E5%B9%B4%E6%98%A5%E5%AD%A3%E5%AD%A6%E6%9C%9F%E7%90%86%E8%AE%BA%E8%AF%BE%E8%AF%84%E6%95%99-%E8%AE%A1%E7%AE%97%E6%9C%BA%E5%8E%9F%E7%90%86")
 
-#driver.get("http://ce.fudan.edu.cn/q.aspx?id=9957450&sqid=b956f4a567ed4ab982a7aede07a4a6da&type=5&stepseq=45ff583eeca240959051a0bf2f11efee&targettype=2&targetcode=869a78ece4374719a3ee006f8661746b&tag=b956f4a567ed4ab982a7aede07a4a6da_5_869a78ece4374719a3ee006f8661746b_2_2019-2020-2-COMP130154_0_9957450&beginTime=2020-04-24%2015:00&endTime=2020-06-14%2023:59&previewTime=2020-03-22%2008:00&name=2020%E5%B9%B4%E6%98%A5%E5%AD%A3%E5%AD%A6%E6%9C%9F%E7%90%86%E8%AE%BA%E8%AF%BE%E8%AF%84%E6%95%99-%E8%AE%A1%E7%AE%97%E6%9C%BA%E5%8E%9F%E7%90%86")
-while 1 :
+while True:
     driver.get("http://ce.fudan.edu.cn/")
-    driver.find_element_by_class_name('evaluateMyTask').click()
+    
+    # Click on the element with class 'evaluateMyTask'
+    driver.find_element(By.CLASS_NAME, 'evaluateMyTask').click()
     time.sleep(1)
+    
+    # Switch to the iframe
     driver.switch_to.frame("iframepage")
-    a = driver.find_element_by_css_selector('#tblQuestion > tbody > tr:nth-child(1) > td.operate > a.mcopen')
+    
+    # Find the first clickable link
+    a = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, '#tblQuestion > tbody > tr:nth-child(1) > td.operate > a'))
+    )
+   
     a.click()
     time.sleep(2)
-    driver.switch_to_window(driver.window_handles[1])
-    radios = driver.find_elements_by_class_name('jqTransformRadioWrapper')
-    checkboxes = driver.find_elements_by_class_name('jqTransformCheckboxWrapper')
-    cnt = 0
+    
+    # Switch to the newly opened window
+    driver.switch_to.window(driver.window_handles[1])
+    
+    # Find all radio button wrappers
+    radios = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, 'jqTransformRadioWrapper'))
+    )
+
+    
+    # Find all checkbox wrappers
+    checkboxes  = WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.CLASS_NAME, 'jqTransformCheckboxWrapper'))
+)
+
+    
+    # Click on all displayed radio buttons in reverse order
+    n=0
     for i in reversed(radios):
-        if i.is_displayed() :
+        n+=1
+        if n >32 and n <=35 :
+            continue
+            
+        if i.is_displayed():
             i.click()
-
-    for i in checkboxes :
-        if i.is_displayed() :
-            cnt = cnt + 1
+            
+    
+    # Click on the first 8 displayed checkboxes and last question's checkbox
+    cnt = 0
+    for i in checkboxes:
+        if i.is_displayed():
+            cnt += 1
             i.click()
-        if cnt >= 8 : break
-
-
-    text = driver.find_elements_by_tag_name('textarea')
-    for i in text :
+        if cnt >= 8:
+            break
+    cnt = 0 
+    for i in checkboxes:
+        if i.is_displayed():
+            cnt += 1
+            
+        if cnt == 20:
+            i.click()
+            break
+    # Find all text areas and fill them with "好"
+    textareas = driver.find_elements(By.TAG_NAME, 'textarea')
+    for i in textareas:
         i.send_keys('好')
 
-    button = driver.find_element_by_id('next_button')
-    button.click()
 
-    buttons = driver.find_elements_by_tag_name('button')
-    for i in buttons :
-        if i.get_attribute('data-id')=='ok' : 
+    # Click the button with id 'next_button'
+    button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, 'next_button'))
+    )
+    button.click()
+    
+    # Find all buttons and click the one with data-id='ok'
+    buttons = driver.find_elements(By.TAG_NAME, 'button')
+    for i in buttons:
+        if i.get_attribute('data-id') == 'ok':
             print('here')
             i.send_keys(Keys.ENTER)
-
+    
+    # Close the current window
     driver.close()
-    driver.switch_to_window(driver.window_handles[0])
+    # Switch back to the main window
+    driver.switch_to.window(driver.window_handles[0])
